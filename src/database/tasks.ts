2c87112e6188bcd3d56ee33b7f439eeaec03d966
mohamadhammoud <mohamad.hammoudk@gmail.com>
2023-07-15 17:18:41 +0300

    🗃️ create dummy database

---
+++ src/database/tasks.ts
@@ -0,0 +1,26 @@
+export default [
+  {
+    id: "1",
+    name: "Work",
+    completed: false,
+    userId: "3",
+  },
+  {
+    id: "2",
+    name: "Eat",
+    completed: true,
+    userId: "1",
+  },
+  {
+    id: "3",
+    name: "Shopping",
+    completed: true,
+    userId: "4",
+  },
+  {
+    id: "4",
+    name: "Gym",
+    completed: true,
+    userId: "2",
+  },
+];
---
+++ src/database/users.ts
@@ -0,0 +1,22 @@
+export default [
+  {
+    id: "1",
+    name: "kevin",
+    email: "kevin@gmail.com",
+  },
+  {
+    id: "2",
+    name: "john",
+    email: "john@gmail.com",
+  },
+  {
+    id: "3",
+    name: "peter",
+    email: "pohn@gmail.com",
+  },
+  {
+    id: "4",
+    name: "bob",
+    email: "bob@gmail.com",
+  },
+];
