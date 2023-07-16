e3b3a2babb768f94920eb878f258c03440872cfe
mohamadhammoud <mohamad.hammoudk@gmail.com>
2023-07-16 15:43:08 +0300

    💥 added create user and get user functionality

--- src/resolvers/user.resolver.ts
+++ src/resolvers/user.resolver.ts
@@ -5,5 +5,23 @@ export default {
     users: () => {
       return users;
     },
+    user: (_: any, { id }: { id: string }) => {
+      const user = users.find((user) => user.id == id);
+      return user;
+    },
+  },
+  Mutation: {
+    createUser: (
+      _: any,
+      { input }: { input: { name: string; email: string } }
+    ) => {
+      users.push({
+        id: `${users.length}`,
+        name: input.name,
+        email: input.email,
+      });
+
+      return users[users.length - 1];
+    },
   },
 };
--- src/type-definitions/user.ts
+++ src/type-definitions/user.ts
@@ -6,6 +6,15 @@ export default gql`
     user(id: ID!): User
   }
 
+  input createUserInput {
+    name: String
+    email: String
+  }
+
+  extend type Mutation {
+    createUser(input: createUserInput): User
+  }
+
   type User {
     id: ID!
     name: String
